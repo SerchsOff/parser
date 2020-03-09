feb74d52e9e8dc32649a8ff45b9952bb4c453df0
Sergey <61414420+SerchsOff@users.noreply.github.com>
2020-03-09 16:55:51 +0300

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+r = requests.get("https://www.eldorado.ru/") #сайт с которого парсить
+print(r.status_code) #статус соединения
+html = BS(r.content, "html.parser")
+
+for el in html.select(".slick-slide"): #элементы которые парсить
+	title = el.select(".zqgg6c-2 > a") #дочерние элементы
+	price = el.select(".zqgg6c-7")
+	
+	try:
+		print(title[0]["title"] + " " + price[0].text) #вывод контента до получения ошибки
+	except IndexError:
+		continue
